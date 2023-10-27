6c16ee428de05cf47192bc5ab6757b2f0d51eb9e
Bogyi95 <117159231+Bogyi95@users.noreply.github.com>
2023-10-27 11:58:34 +0200

    Update cursor cordinate.py

--- cursor cordinate.py
+++ cursor cordinate.py
@@ -4,17 +4,14 @@ import tkinter as tk
 def get_cursor_coordinates(event=None):
     x, y = pyautogui.position()
     coordinates_label.config(text=f"X: {x}, Y: {y}")
-
-# Create a tkinter window
+    
 window = tk.Tk()
 window.title("Cursor Coordinates App")
 
-# Create a label to display coordinates
 coordinates_label = tk.Label(window, text="")
 coordinates_label.pack()
 
-# Bind the "Space" key press to get_cursor_coordinates function
+#Get cursor cordinates after pressing Space button
 window.bind("<space>", get_cursor_coordinates)
 
-# Start the tkinter main loop
 window.mainloop()
